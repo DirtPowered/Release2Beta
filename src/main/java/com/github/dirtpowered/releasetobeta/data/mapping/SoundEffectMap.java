2704238088485ccd7d9bcd880f7d7f2cbab2d19b
outofmem13 <nulln785@gmail.com>
2020-05-14 16:30:20 +0200

    Add missing sound effects/particles

--- src/main/java/com/github/dirtpowered/releasetobeta/data/mapping/SoundEffectMap.java
+++ src/main/java/com/github/dirtpowered/releasetobeta/data/mapping/SoundEffectMap.java
@@ -29,10 +29,14 @@ import com.github.steveice10.mc.protocol.data.game.world.effect.WorldEffect;
 public class SoundEffectMap extends Remapper<WorldEffect> {
 
     public SoundEffectMap() {
+        remap(1000, SoundEffect.BLOCK_DISPENSER_DISPENSE);
+        remap(1001, SoundEffect.BLOCK_DISPENSER_FAIL);
+        remap(1002, SoundEffect.BLOCK_DISPENSER_LAUNCH);
         remap(1003, SoundEffect.BLOCK_WOODEN_DOOR_OPEN);
         remap(1004, SoundEffect.BLOCK_FIRE_EXTINGUISH);
-        remap(1007, SoundEffect.ENTITY_GHAST_WARN);
-        remap(2001, ParticleEffect.BREAK_BLOCK);
         remap(1005, SoundEffect.RECORD);
+        remap(1007, SoundEffect.ENTITY_GHAST_WARN);
+        remap(2000, ParticleEffect.SMOKE);
+        remap(2001, ParticleEffect.BREAK_BLOCK);
     }
 }
