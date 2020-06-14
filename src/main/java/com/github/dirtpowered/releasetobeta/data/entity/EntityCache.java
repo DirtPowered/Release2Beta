3762a60e275d90808fa3321ab66a8a111694c9c8
outofmem13 <nulln785@gmail.com>
2020-06-14 11:30:04 +0200

    fix concurrency issues

--- src/main/java/com/github/dirtpowered/releasetobeta/data/entity/EntityCache.java
+++ src/main/java/com/github/dirtpowered/releasetobeta/data/entity/EntityCache.java
@@ -25,12 +25,12 @@ package com.github.dirtpowered.releasetobeta.data.entity;
 import com.github.dirtpowered.releasetobeta.data.entity.model.Entity;
 import lombok.Getter;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Getter
 public class EntityCache {
-    private Map<Integer, Entity> entities = new HashMap<>();
+    private Map<Integer, Entity> entities = new ConcurrentHashMap<>(); // is accessed by main and client connection thread
 
     public void addEntity(int entityId, Entity entity) {
         entities.put(entityId, entity);
