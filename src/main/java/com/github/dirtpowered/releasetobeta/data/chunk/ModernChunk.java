72945c69197b92228ab21d1a33a4eca6cdefd099
outofmem13 <nulln785@gmail.com>
2020-07-19 13:06:39 +0200

    forgot about that

--- src/main/java/com/github/dirtpowered/releasetobeta/data/chunk/ModernChunk.java
+++ src/main/java/com/github/dirtpowered/releasetobeta/data/chunk/ModernChunk.java
@@ -1,6 +1,7 @@
 package com.github.dirtpowered.releasetobeta.data.chunk;
 
 import com.github.steveice10.mc.protocol.data.game.chunk.Chunk;
+import com.github.steveice10.mc.protocol.data.game.chunk.NibbleArray3d;
 import com.github.steveice10.opennbt.tag.builtin.CompoundTag;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,4 +13,6 @@ import java.util.List;
 public class ModernChunk {
     private Chunk chunk;
     private List<CompoundTag> chunkTileEntities;
+    private NibbleArray3d blockLight;
+    private NibbleArray3d skyLight;
 }
